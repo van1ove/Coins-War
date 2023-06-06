55f3dba685c2c6fe6910c45a37b5e4f314ba3202
van1ove <berezko2004@gmail.com>
2023-06-06 17:10:23 +0300

    camera follows

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private Vector3 _playerVector;
+    [SerializeField] private float _speed;
+
+    private void Update()
+    {
+        _playerVector = _player.position;
+        _playerVector.z = -10;
+        transform.position = Vector3.Lerp(transform.position, _playerVector, _speed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     [SerializeField] private float _moveSpeed = 5f;
+    private float _x, _y, _z;
     private void Start()
     {
         _rb = GetComponent<Rigidbody2D>();
@@ -21,17 +22,13 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        float x = _joystick.Horizontal;
-        float y = _joystick.Vertical;
-        float z = Mathf.Atan2(x, y) * Mathf.Rad2Deg;
-        _rb.velocity = new Vector2(x * _moveSpeed, y * _moveSpeed);
-        //Debug.Log(Quaternion.LookRotation(_rb.velocity));
-        //Debug.Log(_rb.velocity);
-        //Debug.Log(_joystick.Direction);
+        _x = _joystick.Horizontal;
+        _y = _joystick.Vertical;
+        _z = Mathf.Atan2(_x, _y) * Mathf.Rad2Deg;
+        _rb.velocity = new Vector2(_x * _moveSpeed, _y * _moveSpeed);
         if (_joystick.Horizontal != 0 || _joystick.Vertical != 0)
         {
-            transform.eulerAngles = new Vector3(0f, 0f, -z);
-            //transform.rotation = Quaternion.LookRotation(Vector2.up, _rb.velocity);
+            transform.eulerAngles = new Vector3(0f, 0f, -_z);
         }
     }
 }
